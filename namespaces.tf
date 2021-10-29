5a18d388a25e952fff593d15f12b29594ac756c5
Simon Weald <glitchcrab-github@simonweald.com>
2021-10-29 23:21:04 +0100

    add vault auth

--- namespaces.tf
+++ namespaces.tf
@@ -1,4 +1,6 @@
 provider "vault" {
+  address   = hcp_vault_cluster.hcp_vault.vault_public_endpoint_url
+  token     = hcp_vault_cluster_admin_token.setup_token.token
   alias     = "admin"
   namespace = "admin"
 }
@@ -9,6 +11,8 @@ resource "vault_namespace" "operations" {
 }
 
 provider "vault" {
+  address   = hcp_vault_cluster.hcp_vault.vault_public_endpoint_url
+  token     = hcp_vault_cluster_admin_token.setup_token.token
   alias     = "operations"
   namespace = "admin/operations"
 }
