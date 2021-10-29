541e15a583220c5025d349e99dfbd612e60ee622
Simon Weald <glitchcrab-github@simonweald.com>
2021-10-29 14:23:41 +0100

    add backend config

---
+++ backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "remote" {
+    organization = "{{ORGANIZATION_NAME}}"
+
+    workspaces {
+      name = "{{WORKSPACE_NAME}}"
+    }
+  }
+
+  required_version = ">= 0.13.0"
+}
